dbc038f8f7e88f8977c1c8f60404fecc623896cc
RAHIL1221 <rahilcc007@gmail.com>
2026-04-07 16:58:05 +0530

    Update NoteState.jsx

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host =  import.meta.env.VITE_API_URL;
   const [notes, setNotes] = useState([]);
 
   // Fetch all notes
